e063e495ea14f69240243655364daeca24fe3444
Energotika <108826757+Energotika@users.noreply.github.com>
2022-09-19 22:56:24 +0300

    Create DriversInfo.java

---
+++ DriversInfo.java
@@ -0,0 +1,21 @@
+package db.WLT;
+
+public class DriversInfo {
+    //Fields
+    private String dlid;
+    private String pswrd;
+    private String name;
+    private String surname;
+    //Setters and getters
+    public void setDlid(String dlid) {this.dlid = dlid;}
+    public void setPswrd(String pswrd) {this.pswrd = pswrd;}
+    public void setName(String name) {this.name = name;}
+    public void setSurname(String surname) {this.surname = surname;}
+    public String getDlid() {return dlid;}
+    public String getpswrd() {return pswrd;}
+    public String getName() {return name;}
+    public String getSurname() {return surname;}
+
+
+
+}
